bb90740eeb0c86059ba861921a40a9d208ddb0cb
Abdullah Mohamed Saleh <121822230+Abdullah37101@users.noreply.github.com>
2025-08-06 03:44:19 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/fameora-coming-soon">
+      <BrowserRouter basename="/">
   <Routes>
     <Route path="/" element={<Index />} />
     <Route path="*" element={<NotFound />} />
